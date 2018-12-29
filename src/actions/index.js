0eb61d30837043b15ac5a19ac43c025b6c26c320
Animye <hy0620@163.com>
2018-12-29 10:47:59 +0800

    second

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,38 +1,66 @@
 import axios from 'axios'
 import * as actionTypes from '../constants'
 //* as actionTypes 将 constants 内导出的所有东西全部放到 actionTypes 对象内
+// export const getProducts = () => {
+//   return dispatch => {
+//     axios.get('http://localhost:3008/products').then(res => {
+//       dispatch({
+//         type: actionTypes.GET_PRODUCTS,
+//         products: res.data
+//       })
+//     })
+//   }
+// }
 export const getProducts = () => {
   return dispatch => {
-    axios.get('http://localhost:3008/products').then(res => {
-      dispatch({
-        type: actionTypes.GET_PRODUCTS,
-        products: res.data
+    axios
+      .get(
+        'https://raw.githubusercontent.com/Animye/react-redux-cart/master/api/db.json'
+      )
+      .then(res => {
+        dispatch({
+          type: actionTypes.GET_PRODUCTS,
+          products: res.data.products
+        })
       })
-    })
   }
 }
-export const inventoryChange = (id, num) => {
-  return dispatch => {
-    axios.patch(`http://localhost:3008/products/${id}`, num).then(res => {
-      dispatch({
-        type: actionTypes.ADD_TO_CARD,
-        id
-      })
-    })
+// export const inventoryChange = (id, num) => {
+//   return dispatch => {
+//     axios.patch(`http://localhost:3008/products/${id}`, num).then(res => {
+//       dispatch({
+//         type: actionTypes.ADD_TO_CARD,
+//         id
+//       })
+//     })
+//   }
+// }
+export const inventoryChange = id => {
+  return {
+    type: actionTypes.ADD_TO_CARD,
+    id
   }
 }
+
 export const clearCrad = () => {
   return {
     type: actionTypes.CLEAR_CARD
   }
 }
-export const del = (id, num) => {
-  return dispatch => {
-    axios.patch(`http://localhost:3008/products/${id}`, num).then(res => {
-      dispatch({
-        type: actionTypes.DEL,
-        id
-      })
-    })
+export const del = id => {
+  return {
+    type: actionTypes.DEL,
+    id
   }
 }
+
+// export const del = (id, num) => {
+//   return dispatch => {
+//     axios.patch(`http://localhost:3008/products/${id}`, num).then(res => {
+//       dispatch({
+//         type: actionTypes.DEL,
+//         id
+//       })
+//     })
+//   }
+// }
